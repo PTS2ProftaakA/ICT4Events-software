7f1827793bce733426216098b370ea6bc11fbff6
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2015-04-21 19:24:24 +0200

    Added comments

--- ICT4Events-software-Software/Proftaak ICT4Events/Proftaak ICT4Events/Functional Classes/CurrentUser.cs
+++ ICT4Events-software-Software/Proftaak ICT4Events/Proftaak ICT4Events/Functional Classes/CurrentUser.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Proftaak_ICT4Events
 {
+    //This class is used to track the user that is logged in
+    //It is used mostly for convenience but also adds some clarity
     public static class CurrentUser
     {
         public static User currentUser;
